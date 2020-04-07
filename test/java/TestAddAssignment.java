7dfb9e20c04d647afe48cf174c9b11b2c7cc89a6
isabelanazare <isabelaelenanazare@gmail.com>
2020-04-07 17:26:17 +0300

    added addAssignment test cases

--- test/java/TestAddAssignment.java
+++ test/java/TestAddAssignment.java
@@ -19,14 +19,16 @@ public class TestAddAssignment {
     private NotaValidator notaValidator;
     private NotaXMLRepo notaRepository;
     private TemaValidator temaValidator;
-    private TemaXMLRepo temaXMLRepo;
+    private TemaXMLRepo temaRepo;
     private Service service;
 
     @Before
     public void setup() {
         this.studentValidator = new StudentValidator();
+        this.temaValidator = new TemaValidator();
+        this.temaRepo = new TemaXMLRepo("Teme.xml");
         this.studentRepository = new StudentXMLRepo("Studenti.xml");
-        this.service = new Service(studentRepository,studentValidator,temaXMLRepo,temaValidator,notaRepository,notaValidator);
+        this.service = new Service(studentRepository,studentValidator,temaRepo,temaValidator,notaRepository,notaValidator);
     }
 
     @Test
@@ -41,10 +43,69 @@ public class TestAddAssignment {
 
     @Test
     public void testAddAssignmentWithValidReceivingWeek() {
-        Tema tema = new Tema("1","tema 1", 5,3);
+        Tema tema = new Tema("20","tema 1", 5,3);
         try {
             this.service.addTema(tema);
-            assertEquals(this.service.findTema("1"), tema);
+            assertEquals(this.service.findTema("20").getPrimire(), tema.getPrimire());
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignmentWithWithNULLid() {
+        Tema tema = new Tema(null,"tema 1", 5,3);
+        try {
+            this.service.addTema(tema);
+         } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignmentWithWithEmptyid() {
+        Tema tema = new Tema("","tema 1", 5,3);
+        try {
+            this.service.addTema(tema);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignmentWithWithEmptyDescription() {
+        Tema tema = new Tema("40","", 5,3);
+        try {
+            this.service.addTema(tema);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignmentWithWithInvalidDeadline() {
+        Tema tema = new Tema("50","tema 1", 50,3);
+        try {
+            this.service.addTema(tema);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignmentWithWithInvalidReceivingWeek() {
+        Tema tema = new Tema("50","tema 1", 5,30);
+        try {
+            this.service.addTema(tema);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testAddAssignmentWithWitNullEntity() {
+        try {
+            this.service.addTema(null);
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
